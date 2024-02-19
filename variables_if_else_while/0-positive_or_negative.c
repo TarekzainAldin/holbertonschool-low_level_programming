9a2f6a3c31a7385c0594ff6ca9a07e3bd771989a
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-02-19 15:12:33 +0000

    Positive anything is better than negative nothing

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,10 +5,21 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+  int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+	Return (0);
 }
