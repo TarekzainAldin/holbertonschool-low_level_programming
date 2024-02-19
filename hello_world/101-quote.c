8f662ae08d3ce59cfc6c896d83b2a663e94e05b8
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-02-19 13:10:14 +0000

    UNIX is basically

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
  * main- Entry point
- * @void: parameter
- * Return: 1 (success)
- **/
+ *
+ * Description: A program that prints to standard error
+ *
+ * Return: Always 1 (success)
+ */
 int main(void)
 {
-printf(stderr, "and that piece of art is useful\" - \Dora Korpar, 2015-10-19\n");
-return (1);
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
