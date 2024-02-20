b8595ab18b31ae25210c75d3f18f0d9222c08a1a
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-02-20 13:45:51 +0000

    I sometimes suffer

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * print_alphabet - Entry point
- * Return:0
- */
+* print_alphabet - Entry point
+* Return:0
+*/
 void print_alphabet(void)
 {
- char letter;
- for (letter = 'a'; letter <= 'z'; letter++)
- {
- _putchar(letter);
- }
- _putchar('\n');
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');
 }
