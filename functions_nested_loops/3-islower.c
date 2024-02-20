9bace0e680e8839e595ebc1151363da81a795a14
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-02-20 14:29:35 +0000

    is lower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * _islower - checks for lowercase character
 * Return: 1 if c is lowercase, 0 otherwise
@@ -8,8 +7,7 @@ int _islower(int c)
 {
 if (c >= 97 && c <= 122)
 {
-  
 return (1);
 }
-return(0);
+return (0);
 }
