5f4509756458bb19291a74de6c6b9c57ed578054
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-02-20 12:52:38 +0000

    void print_alphabet(void);

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include<main.h>
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+void print_alphabet(void)
+{
+  char letter ;
+  for(letter ='a';letter <'z';letter++)
+    putchar(letter);
+  putchar("\n")
+    return 0;
+}
