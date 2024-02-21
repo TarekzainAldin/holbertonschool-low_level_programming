3c3677d2688094147e8c02b1f97fe19739a230a9
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-02-21 09:09:26 +0000

    abs

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -5,17 +5,16 @@
 *
 * Return: Absolute value of number or zero
 */
-int_abs(int c)
+int _abs(int c)
 {
-if (c < 0)
+ if (c < 0)
 {
 int abs_val;
-abs_val= c * -1 ;
+ abs_val = c * -1;
 return (abs_val);
 }
 return (c);
 }
-
 
 
 
