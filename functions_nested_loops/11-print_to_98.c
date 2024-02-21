c6920572c9c93ec8d2fac968bbbecb4fa7d24c74
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-02-21 13:26:59 +0000

    print number from n to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -3,12 +3,11 @@
 /**
 * print_to_98 - Prints all natural numbers from n to 98
 * @n: The number to start printing from
-
 * Return: Always 0.
 */
 void print_to_98(int n)
 {
-    int i;    
+  int i;
 	if (n <= 98)
 	{
 		for (i = n ; i <= 98 ; i++)
