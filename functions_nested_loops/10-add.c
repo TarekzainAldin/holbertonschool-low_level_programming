2d5e786b39a388935f12e7c8d3786e7459e20693
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-02-21 11:58:28 +0000

    the add number

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,8 +1,12 @@
 #include "main.h"
 /**
-*add tow number and return the rusult
-*/
-int add(int a,int b)
+  * add - Adds two values
+  * @n1: The first number to be computed
+  * @n2: The second number to be computed
+  *
+  * Return: Integer value
+  */
+int add(int a , int b)
 {
 return (a + b);
 }
