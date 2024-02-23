d955a2842fe7f285513f459fe0e0b398965a44e1
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-02-23 14:12:33 +0000

    fibonacci number

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 /**
- *main - thise f number 
+ *main - thise f number
  *
  *the varaible numf& and numF
- */ 
-
-	int main(void)
-	{
-		int count;
-		unsigned long numf1 = 0, numf2 = 1, sum;
-		for (count = 0; count < 50; count++)
-		{
-			sum = numf1 + numf2;
-			printf("%lu", sum);
-			numf1 = numf2;
-			numf2 = sum;
-			if (count == 49)
-				printf("\n");
-			else
-				printf(", ");
-		}
-
-		return (0);
-	}
+ */
+int main(void)
+{
+int count;
+unsigned long numf1 = 0, numf2 = 1, sum;
+for (count = 0; count < 50; count++)
+{
+sum = numf1 + numf2;
+printf("%lu", sum);
+numf1 = numf2;
+numf2 = sum;
+if (count == 49)
+printf("\n");
+else
+printf(", ");
+}
+return (0);
+}
