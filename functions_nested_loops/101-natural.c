26f9f9caae0c6b6b25c16e488264fb24c71b3382
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-02-23 11:29:22 +0000

    the multiplation number 1024 at 3 or 5

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -5,14 +5,14 @@
  */
 int main(void)
 {
-  int i=0;
-    int z =0;
-    while(i <1024)
+  int i= 0;
+    int z = 0;
+    while(i < 1024)
       {
 	if ((i % 3 == 0) || (i % 5 ==0))
 	  {
 
-	    z ++= i;
+	    z += i;
 	  }
 	i++;
       }
