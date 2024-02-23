de74d4f2bfc430174cf2a480c5e53c1f2a967a3b
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-02-23 13:50:01 +0000

    fibonacci number

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -1,10 +1,14 @@
-#include <iostream>
-using namespace std; 
+#include <stdio.h>
+/**
+ *main - thise f number 
+ *
+ *the varaible numf& and numF
+ */ 
 
 	int main(void)
 	{
 		int count;
-		unsigned long numf1 = 0, numf2 = 1, sum;
+		int numf1 = 0, numf2 = 1, sum;
 		for (count = 0; count < 50; count++)
 		{
 			sum = numf1 + numf2;
