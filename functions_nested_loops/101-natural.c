4558f7da9e3536ef3313b14bb8c38ceb6624d5e0
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-02-23 12:33:44 +0000

    the multiplation number 1024 at 3 or 5

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-* main - Nature made the natural numbers sum of all multiples of 3 or 5 up to 1024
-*Find the sum of all the multiples of 3 or 5 below 1000
+* main - Nature made the natural numbers sum of all multiples
+*Find the sum of all the multiples
 * Return:Always (0)
 */
 int main(void)
