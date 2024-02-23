95c58dbbf0a90a3ffce7048509550ddb55c78261
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-02-23 11:23:59 +0000

    the multiplation number 1024 at 3 or 5

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+inclue <stdio.h>
+/**
+ *main Nature made the natural numbers sum of all multiples of 3 or 5 up to 1024
+ * Return:Always (Success)
+ */
+int main void (void)
+{
+  int i=0;
+    int z =0;
+    while(i <1024)
+      {
+	if ((i % 3 == 0) || (i % 5 ==0))
+	  {
+
+	    z ++= i;
+	  }
+	i++;
+      }
+    printf("%d\n",z
+	
+  return (0)
+}
