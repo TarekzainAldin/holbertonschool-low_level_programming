d74a8833de9a58320cc781e5f577480c98a9353e
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-02-27 11:52:44 +0000

    reverse the string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,17 +6,12 @@
  */
 void rev_string(char *s)
 {
-   char ch, *p, *q;
-
-  for (q = s; *q != '\0'; ++q)
-    ;
-  if (q > s)
-    --q;
-  
-
-  for (p = s; p < q; ++p, --q) {
-    ch = *p;
-    *p = *q;
-    *q = ch;
-  }
+char ch, *p, *q;
+for (q = s; *q != '\0'; ++q)
+if (q > s)
+--q;
+for (p = s; p < q; ++p, --q)
+ch = *p;
+*p = *q;
+*q = ch;
 }
