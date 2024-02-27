2d4e459f020945af6bc9e17c8a8cc2ee16f8d1ac
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-02-27 12:55:51 +0000

    print the even number

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,11 @@
  */
 void puts2(char *s)
 {
-	int a;
-	for (a = 0; s[a] != '\0'; a++)
-	{
-		if (a % 2 == 0)
-		{
-			_putchar (s[a]);
-		}
-	}
-	_putchar ('\n');
+int a;
+for (a = 0; s[a] != '\0'; a++)
+if (a % 2 == 0)
+{
+_putchar (s[a]);
+}
+_putchar ('\n');
 }
