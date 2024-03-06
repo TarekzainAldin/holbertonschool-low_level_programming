1bf19f76bcf9b90c8696fd75bcd2c602537c24f0
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-03-06 08:46:27 +0000

    edit the code for the prime number task 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * is_prime_number - checks if a number is prime recursively
  * @n: the number to check
@@ -8,11 +7,11 @@
  */
 int is_prime_number(int n)
 {
-    if (n <= 1)
-        return (0);
-    if (n % 2 == 0 && n > 2)
-        return (0);
-    if (n == 2)
-        return (1);
-    return (is_prime_number(n - 2) || is_prime_number(n - 1));
+if (n <= 1)
+return (0);
+if (n % 2 == 0 && n > 2)
+return (0);
+if (n == 2)
+return (1);
+return (is_prime_number(n - 2) || is_prime_number(n - 1));
 }
