b0fe8fd26c5521e7e6fb8393fdcbc288e77acc20
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-03-06 08:56:11 +0000

    edit the code for the first task

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -6,9 +6,9 @@
   * @argv: argument vector
   *
   */
-int main (int argc , char * argv [] )
+int main(int argc, char *argv[])
 {
-  (void) argc;
-  printf("%s \n" , argv[0]);
-	return (0);
+(void) argc;
+printf("%s \n", argv[0]);
+return (0);
 }
