cf7a99b5149eacc6c9ab2329ad04e5355d9e8d3b
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-03-06 09:18:12 +0000

    edit the code in task

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,19 +8,19 @@
   *
   * Return: Always zero
   */
-int main ( int argc , char *argv[])
+int main(int argc, char *argv[])
 {
-  int num1 = 0 , num2 = 0 ;
-  if (argc == 3)
-    {
-      num1 = atoi (argv [1]);
-      num2 = atoi (argv [2]);
-      printf ( "%d\n", num1 * num2);
-    }
-  else
-    {
-      printf("Error\n");
-      return (1);
-    }
-  return (0);
+int num1 = 0, num2 = 0;
+if (argc == 3)
+{
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+printf("%d\n", num1 *num2);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
 }
