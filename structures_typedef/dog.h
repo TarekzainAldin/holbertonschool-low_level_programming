c610be4fd351cbe1d157ff12c6a9fffc56b6823a
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-03-12 13:38:09 +0000

    edit the poppy in the first task int the project

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,14 +1,17 @@
-#include <stdio.h>
+#ifndef dog_h
+#define dog_h
 /**
- * struct dog - Short description
- * @name: First member
- * @age: Second member
- * @owner: Third member
+ * struct dog - dog data
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
  *
- * Description: Longer description
+ * Description: Defines metadata about the dog and type of dog's struct
  */
-struct dog {
-  char *name;
-    float age;
-    char *owner;
+struct dog
+{
+char *name;
+float age;
+char *owner;
 };
+#endif /* dog_h */
