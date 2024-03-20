0f60e08588737c1d3c7784121e04c8cad614e023
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-03-20 08:43:10 +0000

    edit the bettty code

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-
 /**
   * print_name - Prints a name
   * @name: The name to prints
@@ -9,7 +8,5 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL)
-	  return;
-		f(name);
+(*f)(name);
 }
