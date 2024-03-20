40b542244ba9e6cbe27204c59ee29a2acd61b041
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-03-20 08:16:27 +0000

    edit the bettty code

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,14 @@
 #include "function_pointers.h"
+
 /**
- * print_name - print name from a function pointer
- * @name: char string
- * @f: function pointer take string parmeters
- */
+  * print_name - Prints a name
+  * @name: The name to prints
+  * @f: Pointer to function
+  *
+  * Return: Nothing
+  */
 void print_name(char *name, void (*f)(char *))
 {
-f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
