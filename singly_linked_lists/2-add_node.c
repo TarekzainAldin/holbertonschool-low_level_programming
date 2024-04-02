05493cc2af202b168bd5d5e97068da84d3b9f0fb
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-04-02 08:25:43 +0000

    edit the code in the task

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,23 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "lists.h"
+
 /**
- * add_node - add a node to a singly linked list
- * @head: points to the start of a list
- * @str: string to put in the element
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: Pointer to the pointer to the head node.
+ * @str: String to be added to the new node.
  *
- * Return: the address of the new node
+ * Return: Address of the new element, or NULL if it failed.
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_head = malloc(sizeof(list_t));
+    int i = 0;
+    list_t *new_node;
 
-	if (head == NULL || new_head == NULL  || str == NULL)
-	return (0);
+    /* Count the length of the string */
+    while (str[i])
+        i++;
 
-	new_head->str = strdup(str);
-	new_head->len = strlen(str);
-	new_head->next = *head;
-	*head = new_head;
+    /* Allocate memory for the new node */
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	return (new_head);
+    /* Allocate memory and copy the string */
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    /* Assign the length and set the next pointer */
+    new_node->len = i;
+    new_node->next = *head;
+
+    /* Update the head pointer to point to the new node */
+    *head = new_node;
+
+    return (new_node);
 }
-
